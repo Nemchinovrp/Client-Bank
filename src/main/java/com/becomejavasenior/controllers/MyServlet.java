b0a7040e8988097a483fa43ae872b8da20d7e2a7
nemchinovrp <nemchinovrp@yandex.ru>
2016-01-25 13:30:11 +0800

    servlet

---
+++ src/main/java/com/becomejavasenior/controllers/MyServlet.java
@@ -0,0 +1,37 @@
+package com.becomejavasenior.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Роман on 24.01.2016.
+ */
+public class MyServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html><head>");
+            out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+            out.println("<title>Hello, World</title></head>");
+            out.println("<body>");
+            out.println("<h1>Hello, world!</h1>");
+            out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+            out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+            out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+}
