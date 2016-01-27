033a261731bc44d7331bd1d49835cfbe32e6d63c
nemchinovrp <nemchinovrp@yandex.ru>
2016-01-27 10:25:28 +0800

    servlet file

---
+++ src/main/java/com/becomejavasenior/controllers/CurrencyController.java
@@ -0,0 +1,26 @@
+package com.becomejavasenior.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Роман on 24.01.2016.
+ */
+public class CurrencyController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
